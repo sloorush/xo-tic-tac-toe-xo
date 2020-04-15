2308bfa6039116d8c4009e8bab1077f6041264ae
rush <aarush.bhatt@gmail.com>
2020-04-16 02:09:27 +0530

    Updates

---
+++ precache-manifest.54bc6c8bec75e0c7976b27ea1a88d741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eacd8bd95d98950a6b93f3dbbfed9a8c",
+    "url": "/xo-tic-tac-toe-xo/index.html"
+  },
+  {
+    "revision": "5ec5e855852d605bf5a6",
+    "url": "/xo-tic-tac-toe-xo/static/css/main.60416a1c.chunk.css"
+  },
+  {
+    "revision": "0be463a7ebd1fca3142c",
+    "url": "/xo-tic-tac-toe-xo/static/js/2.a2116fd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/xo-tic-tac-toe-xo/static/js/2.a2116fd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ec5e855852d605bf5a6",
+    "url": "/xo-tic-tac-toe-xo/static/js/main.b9541e6b.chunk.js"
+  },
+  {
+    "revision": "3b451f8922acb84a2e05",
+    "url": "/xo-tic-tac-toe-xo/static/js/runtime-main.2eda3418.js"
+  }
+]);
