2153e459a814443a1b3e28c980e2d8e17040099b
rush <aarush.bhatt@gmail.com>
2020-04-16 02:02:39 +0530

    added states

--- src/components/Board.js
+++ src/components/Board.js
@@ -12,15 +12,10 @@ const style={
 
 const Board =({squares, onClick})=>(
         <div style={style}>
-            <Square value='1' onClick={()=> onClick('dummy')} />
-            <Square value='2' onClick={()=> onClick('dummy')} />
-            <Square value='3' onClick={()=> onClick('dummy')} />
-            <Square value='4' onClick={()=> onClick('dummy')} />
-            <Square value='5' onClick={()=> onClick('dummy')} />
-            <Square value='6' onClick={()=> onClick('dummy')} />
-            <Square value='7' onClick={()=> onClick('dummy')} />
-            <Square value='8' onClick={()=> onClick('dummy')} />
-            <Square value='9' onClick={()=> onClick('dummy')} />
+            {squares.map((square,i)=>(
+                <Square key={i} value={square} onClick={()=> onClick(i)} />
+            )
+            )}
         </div>
 )
 
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Board from './Board';
+import {calculateWinner} from '../helpers'
 
 const Game =()=>{
-    const handleClick=()=>{
+    const [board, setBoard]=useState(Array(9).fill(null));
+    const [xIsNext, setXisNext]=useState(true);
+    const winner = calculateWinner(board);
 
+    const styles = {
+        width: '200px',
+        margin: '20px auto',
+    };
+    
+    const handleClick=i=>{
+        const boardCopy=[...board];
+        if(winner || boardCopy[i]) return;
+        boardCopy[i]=xIsNext?'X':'O';
+        setBoard(boardCopy);
+        setXisNext(!xIsNext);
     }
     
     const jumpTo = () => {
         
     }
     
-    const renderMoves = () => {
-        
-    }
+    const renderMoves = () =>
+        (<button onClick={()=> setBoard(Array(9).fill(null))}>
+        Start!!!
+    </button>)
     
     return(
-        <div>
+        <div style={{display:'flex',flexDirection:'column', alignItems:'center'}}>
             <h1 style={{fontSize:'50px', textAlign:'center'}}> xo-tic-tac-toe-xo</h1>
-            <Board onClick={handleClick} />
+            <Board squares={board} onClick={handleClick} />
+            <div styles={styles}>
+                <h1>{winner?'We have a Winner '+winner:'Next Move '+ (xIsNext ? 'X' : 'O')}</h1>
+                {renderMoves()}
+            </div>
         </div>
     )
-
 }
 
 export default Game
